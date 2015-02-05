b5b5427b4614a7aa554444cca10b3776921f8c2b
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2015-02-04 23:11:30 -0400

    update google access token in db on user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  require 'GDriveManager'
 
   def new
   end
@@ -9,6 +10,8 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:password])
       #save the user id in a cookie (this allow the user stay logged in the site)
       session[:user_id] = @user.id
+      puts GDriveManager.get_new_google_access_token(@user.google_refresh_token)
+      @user.update_attribute :google_access_code, GDriveManager.get_new_google_access_token(@user.google_refresh_token)["access_token"]
       #flash[:notice] = "Welcome back #{@user.username}"
       redirect_to "/files/index"
     else
--- lib/GDriveManager.rb
+++ lib/GDriveManager.rb
@@ -1,4 +1,5 @@
 require 'google/api_client'
+require 'net/http'
 
 class GDriveManager
   attr_accessor :client
@@ -10,15 +11,17 @@ class GDriveManager
     @client.authorization.scope = 'https://www.googleapis.com/auth/drive' 
   end
 
-  def get_new_google_access_token client_id, client_secret, refresh_token, grant_type='refresh_token'
+  def self.get_new_google_access_token refresh_token, grant_type='refresh_token'
+    data = JSON.parse(File.read("/home/teodoro/Documents/Projects/RubyProjects/las_nubes/client_secret.json"))
     uri = URI('https://www.googleapis.com/oauth2/v3/token')
-    response = Net::HTTP.post_form(uri, 'client_id' => client_id, 'client_secret' => client_secret, 'refresh_token' => refresh_token, 'grant_type' => 'refresh_token')
+    response = Net::HTTP.post_form(uri, 'client_id' => data["web"]["client_id"], 'client_secret' => data["web"]["client_secret"], 
+                                   'refresh_token' => refresh_token, 'grant_type' => grant_type)
     return JSON.parse(response.body)
   end
   
   def upload_to_gdrive a_file, google_access_code
-    client.authorization.access_code = google_access_code
-    drive = client.client.discovered_api('drive', 'v2')
+    client.authorization.access_token = google_access_code
+    drive = client.discovered_api('drive', 'v2')
     file = Google::APIClient::UploadIO.new(a_file, 'image/png')
 
     metadata = {
@@ -39,6 +42,8 @@ class GDriveManager
     puts result.data.to_s
   end
 end
+
+#puts GDriveManager.get_new_google_access_token("1/0i9klxuCqICiwknXiw0IpPEl_GXdOMcj4YMGC2ROB-EMEudVrK5jSpoR30zcRFq6")
 =begin
 puts l.get_new_google_access_token('180099244229-i2mfb4k2tj022615ripnkegqep3n4t15.apps.googleusercontent.com',
                                    'kOd5dswJJE6tfBI0cCa5oI6R',
